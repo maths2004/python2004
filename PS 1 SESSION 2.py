c5abf7ee33fcba8a558e4ff01038ae74a1f38a2c
prakharpython <138801578+maths2004@users.noreply.github.com>
2023-07-31 15:12:19 +0530

    Add files via upload

---
+++ PS 1 SESSION 2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### print your name 
+
+# In[10]:
+
+
+name="Prakharbhatnagar"
+name
+
+
+# ### datatype of your name
+
+# In[13]:
+
+
+a="prakhar"
+type(a)
+
+
+# ### print your age
+
+# In[14]:
+
+
+my_age = 19
+my_age
+
+
+# ### datatype of age
+
+# In[16]:
+
+
+a=19
+type(a)
+
+
+# In[ ]:
+
+
+
+
