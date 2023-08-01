b472263c5edbcb9c62d943ff0ca761698ce3edf3
prakharpython <138801578+maths2004@users.noreply.github.com>
2023-08-01 16:24:30 +0530

    Add files via upload

---
+++ SESSION 3 ..py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### SAMPLE_LIST
+
+# In[3]:
+
+
+sample_list=[1,2,3,4,5,6]
+sample_list
+
+
+# In[4]:
+
+
+type (sample_list)
+
+
+# In[5]:
+
+
+sample_list[1]
+
+
+# In[7]:
+
+
+sample_list[3]=12
+sample_list
+
+
+# ### sample_tuple
+
+# In[8]:
+
+
+sample_tuple=(1,2,3,4,5,6)
+sample_tuple
+
+
+# In[9]:
+
+
+type (sample_tuple)
+
+
+# In[10]:
+
+
+sample_tuple[1]
+
+
+# ### sample_set
+
+# In[13]:
+
+
+sample_set={5,10,15,20,"iiii","aaaa", "ppp"}
+sample_set
+
+
+# In[14]:
+
+
+sample_set.add(123)
+sample_set
+
+
+# In[17]:
+
+
+sample_set.remove("aaaa")
+sample_set
+
+
+# ### dictionary
+
+# In[18]:
+
+
+sample_dict={1:54,2:23,3:69}
+sample_dict
+
+
+# In[20]:
+
+
+sample_dict[3]
+
+
+# In[21]:
+
+
+sample_dict[4]=100
+sample_dict
+
+
+# In[23]:
+
+
+sample_dict[13]="prakhar"
+sample_dict
+
+
+# In[ ]:
+
+
+
+
