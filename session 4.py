9893e84b4c525f94305c59ce9072a7519fefada4
prakharpython <138801578+maths2004@users.noreply.github.com>
2023-08-09 23:34:34 +0530

    Add files via upload

---
+++ session 4.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+simple_list=[1,2,3] 
+np.array(simple_list)
+
+
+# In[11]:
+
+
+list_of_lists=[[1,2,3],[2,5,6],[5,6,9]]
+np.array (list_of_lists)
+
+
+# In[13]:
+
+
+np.arange(1,31,5)
+
+
+# In[14]:
+
+
+np.arange(5)
+
+
+# In[21]:
+
+
+np.zeros  (100,int)
+
+
+# In[24]:
+
+
+np.ones ((2,3),int)
+
+
+# In[25]:
+
+
+np.ones((1000),int)
+
+
+# In[ ]:
+
+
+
+
